cf585fc7581a42d3808f07af96037d70b1d8a953
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-04-04 05:16:04 +0530

    in rotated sorted array

---
+++ search/mini.py
@@ -0,0 +1,16 @@
+#find minimum in rotated array
+
+nums=[] #given
+return min(nums)
+
+#using binary search 
+
+left=0
+right=len(nums)-1
+while left<right:
+	mid=left+(right-left)//2
+	if nums[mid]>nums[right]:
+			left=mid+1
+	else:
+		right=mid
+return nums[left]
