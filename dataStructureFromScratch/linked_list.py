3eef27a6f38b5cdcb80e7847b04f8b7743fad5a0
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-06-28 15:38:50 +0530

    linked list from scratch

--- dataStructureFromScratch/linked_list.py
+++ dataStructureFromScratch/linked_list.py
@@ -10,7 +10,32 @@ class LinkedList:
 		new_node.next=self.head
 		self.head=new_node
 	def insert_at_end(self , data):
-		new_node=Node(data)
+                new_node=Node(data)
+               	current=self.head
+               	if self.head is None:
+                       	self.head=new_node
+                       	return 
+                while(current.next!=None):
+                        current=current.next
+               	current.next=new_node
+	def delete_at_beginning(self):
+                current=self.head
+                if self.head is None:
+                        return
+                self.head=self.head.next
+               	current.next=None
+	def delete_at_end(self):
+               	if self.head is None:
+                       	return
+               	current=self.head
+               	if self.head.next is None:
+                       	self.head=None 
+                       	return
+                while current.next.next!=None :
+                               	current=current.next
+                tofree=current.next
+                current.next=None
+                tofree.next=None
 	def display(self):
 		current=self.head 
 		if self.head is None:
@@ -21,5 +46,9 @@ class LinkedList:
 			current=current.next
 ll=LinkedList()
 ll.insert_at_beginning(5)
+ll.insert_at_beginning(6)
+ll.insert_at_end(4)
+ll.delete_at_end()
+ll.delete_at_beginning()
 ll.display()
 
