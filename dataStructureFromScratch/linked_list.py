cd278c847bfb6709c3a6733363421193a349de80
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-07-01 19:27:04 +0530

    linked list from scratch

--- dataStructureFromScratch/linked_list.py
+++ dataStructureFromScratch/linked_list.py
@@ -52,6 +52,26 @@ class LinkedList:
                 tofree=current.next
                 current.next=None
                 tofree.next=None
+	def delete_at_position(self,position):
+                current=self.head
+                if self.head is None :
+                        print("Linked list is empty.")
+                        return
+                counter=0
+                if position==0:
+                        self.delete_at_beginning()
+                        return 
+                while current is not None:
+                                if counter==position-1:
+                                        if current.next is None:
+                                                        print("Position out of range")
+                                                        return
+                                        tofree=current.next
+                                        current.next=current.next.next
+                                        tofree.next=None
+                                        return
+                                current=current.next
+                                counter+=1
 	def display(self):
 		current=self.head 
 		if self.head is None:
@@ -79,7 +99,8 @@ ll.insert_at_beginning(6)
 ll.insert_at_end(4)
 #ll.delete_at_end()
 #ll.delete_at_beginning()
-ll.insert_at_position(3,0)
+#ll.insert_at_position(3,2)
+ll.delete_at_position(3)
 ll.display()
 
 
